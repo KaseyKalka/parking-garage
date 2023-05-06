61092b2677ab16adcc6b1dbf71ab7f3ee596acde
KaseyKalka <kaseykalka@gmail.com>
2023-05-06 18:56:04 -0500

    added logic to pay_for_parking

--- parking_garage/parking_garage.py
+++ parking_garage/parking_garage.py
@@ -1,25 +1,36 @@
 class Parking_Garage(): 
 
     def __init__(self, tickets, parkingSpaces, currentTicket):
-        self.tickets = tickets # list
-        self.parkingSpaces = parkingSpaces # list
-        self.currentTicket = currentTicket # dictionary
+        self.tickets = [] # list
+        self.parkingSpaces = [] # list
+        self.currentTicket = {'Paid': False} # dictionary
 
-    def take_ticket():
-        pass
+    def take_ticket(self):
+        num_tickets = list(range(1,100))
+        
     # Decrease the amount of tickets available by 1
     # Decrease the amount of parking spots available by 1
+        
+    def pay_for_parking(self):     
+        # Displays an input that waits for  an amount from the user and store it in a variable
+        payment = float(input("Please pay $10.00 for your parking ticket: "))
+
+        # If the payment variable is not empty then (meaning the ticket has been paid) -> 
+        # display a message to the user that their ticket has been paid and they have 15mins to leave
+        while True:
+            if payment == 10.00:
+                print('Your ticket has been paid. You have 15 minutes to exit the garage.')
+                this_ticket = self.currentTicket
+                # Update the 'currentTicket' dictionary key 'paid' to True
+                this_ticket['Paid'] == True
+                self.currentTicket = this_ticket
+                break
+            # Need an else statement that will start this loop from beginning if payment does not equal 10.00
+                
+                
     
-    def pay_for_parking():
-        pass
-    # Displays an input that waits for  an amount from the user and store it in a variable
 
-    # If the payment variable is not empty then (meaning the ticket has been paid) -> 
-    # display a message to the user that their ticket has been paid and they have 15mins to leave
-
-    # Update the 'currentTicket' dictionary key 'paid' to True
-
-    def leave_garage():
+    def leave_garage(self):
         pass
 
     # If ticket has been paid, display 'Thank you, have a nice day'
@@ -27,3 +38,9 @@ class Parking_Garage():
     # Once paid display, 'Thank you, have a nice day'
     # Update parkingSpaces list to increase by 1
     # Update tickets list to increase by 1
+
+
+num_tickets = []
+num_spots = []
+var = Parking_Garage(num_tickets, num_spots, False)
+var.pay_for_parking()
