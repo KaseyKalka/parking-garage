85168aaa2ccdf6b3fe943dfd838bf766a64e8222
KaseyKalka <kaseykalka@gmail.com>
2023-05-06 11:46:59 -0500

    first commit

---
+++ parking_garage/parking_garage.py
@@ -0,0 +1,29 @@
+class Parking_Garage(): 
+
+    def __init__(self, tickets, parkingSpaces, currentTicket):
+        self.tickets = tickets # list
+        self.parkingSpaces = parkingSpaces # list
+        self.currentTicket = currentTicket # dictionary
+
+    def this_ticket():
+        pass
+    # Decrease the amount of tickets available by 1
+    # Decrease the amount of parking spots available by 1
+    
+    def pay_for_parking():
+        pass
+    # Displays an input that waits for  an amount from the user and store it in a variable
+
+    # If the payment variable is not empty then (meaning the ticket has been paid) -> 
+    # display a message to the user that their ticket has been paid and they have 15mins to leave
+
+    # Update the 'currentTicket' dictionary key 'paid' to True
+
+    def leave_garage():
+        pass
+
+    # If ticket has been paid, display 'Thank you, have a nice day'
+    # If ticket hasn't been paid, display an input prompt for payment
+    # Once paid display, 'Thank you, have a nice day'
+    # Update parkingSpaces list to increase by 1
+    # Update tickets list to increase by 1
